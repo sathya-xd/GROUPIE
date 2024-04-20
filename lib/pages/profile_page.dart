5b1dee1f79d58d04d0ed43be12715c4606870f88
sathya pramod <spdp2003@gmail.com>
2024-04-20 20:18:44 +0530

    First commit

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,216 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:groupchat/pages/auth/login_page.dart';
+import 'package:groupchat/pages/home_page.dart';
+import 'package:groupchat/services/auth_service.dart';
+import 'package:groupchat/widgets/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+
+// ignore: must_be_immutable
+class ProfilePage extends StatefulWidget {
+  String userName;
+  String email;
+  ProfilePage({Key? key, required this.email, required this.userName})
+      : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  AuthService authService = AuthService();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  File? _image; // Make _image nullable
+  final picker = ImagePicker();
+
+  Future<void> _getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+      preferredCameraDevice: CameraDevice.front,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _uploadProfilePicture() async {
+    try {
+      if (_image != null) {
+        String userId = _auth.currentUser!.uid;
+        Reference ref = _storage.ref().child('profile_pictures/$userId.jpg');
+        await ref.putFile(_image!);
+        String imageUrl = await ref.getDownloadURL();
+
+        // TODO: Update the user's profile in Firebase with the imageUrl
+        // For example, you can use Firestore to store user data including the profile picture URL.
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .update({'profilePicture': imageUrl});
+
+        // Display a success message or update UI accordingly.
+        print('Profile picture uploaded successfully!');
+      } else {
+        // Handle case where no image is selected.
+        print('No image selected');
+      }
+    } catch (e) {
+      // Handle errors during the upload process.
+      print('Error uploading profile picture: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        title: const Text(
+          "Profile",
+          style: TextStyle(
+              color: Colors.white, fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: Drawer(
+          child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 50),
+        children: <Widget>[
+          InkWell(
+            onTap: _getImageFromGallery,
+            child: CircleAvatar(
+              radius: 120,
+              backgroundImage: _image != null ? FileImage(_image!) : null,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            widget.userName,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Divider(
+            height: 2,
+          ),
+          ListTile(
+            onTap: () {
+              nextScreen(context, const HomePage());
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Groups",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            selected: true,
+            selectedColor: Theme.of(context).primaryColor,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.group),
+            title: const Text(
+              "Profile",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          ListTile(
+            onTap: () async {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Logout"),
+                      content: const Text("Are you sure you want to logout?"),
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.cancel,
+                            color: Colors.red,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            await authService.signOut();
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginPage()),
+                                (route) => false);
+                          },
+                          icon: const Icon(
+                            Icons.done,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text(
+              "Logout",
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
+      )),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 170),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onDoubleTap: () {},
+              child: Icon(
+                Icons.account_circle,
+                size: 200,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Full Name", style: TextStyle(fontSize: 17)),
+                Text(widget.userName, style: const TextStyle(fontSize: 17)),
+              ],
+            ),
+            const Divider(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Email", style: TextStyle(fontSize: 17)),
+                Text(widget.email, style: const TextStyle(fontSize: 17)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
